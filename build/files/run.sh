809385ffc6e81770627a8faf95f468f03346a08d
ueno.s <ueno.s@gamestudio.co.jp>
2024-06-26 19:17:12 +0900

    update images

--- build/files/run.sh
+++ build/files/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+sed -i -e "s/ssl:helixcore:1666/${P4PORT}/g" /etc/perforce/p4dctl.conf.d/p4p-master.conf
+
+p4 trust -y -f
+yes $P4PASSWD | p4 login
+sudo -E -u perforce yes $P4PASSWD | p4 login
+cat /root/.p4trust > /opt/perforce/.p4trust
+cat /root/.p4tickets > /opt/perforce/.p4tickets
+
+sudo service cron start
+sudo -E -u perforce p4dctl start -t p4p p4p-master
+
 exec /usr/bin/tail -f ${P4PLOGFILE}
